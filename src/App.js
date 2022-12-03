b34e5bb472752855543d07d998474cef9a83381f
LimNoah <limnoah0301@gmail.com>
2022-12-04 02:15:15 +0900

    feat: Add BasicTextFields at App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import TemporaryDrawer from "./components/TemporaryDrawer";
 import ResponsiveDrawer from "./components/ResponsiveDrawer";
+import BasicTextFields from "./components/BasicTextField";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <hr />
         <TemporaryDrawer />
         <ResponsiveDrawer />
+        <BasicTextFields />
       </header>
     </div>
   );
